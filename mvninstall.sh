94c9242469117d9e7213b65471d9ed700208d6c9
Christian Stocker <me@chregu.tv>
2011-05-30 22:48:55 +0200

    initial commit

---
+++ mvninstall.sh
@@ -0,0 +1,2 @@
+export MAVEN_OPTS=-Xmx512m 
+mvn install -Dmaven.test.skip=true
---
+++ upload_to_S3.sh
@@ -0,0 +1,2 @@
+php uploadFile.php jackrabbit-standalone/target/jackrabbit-standalone-2.2.7-jackalope-SNAPSHOT.jar jackrabbit-standalone-2.2.7-jackalope-SNAPSHOT.jar "application/x-gzip"
+php uploadFile.php index.html index.html
