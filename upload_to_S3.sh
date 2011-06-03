29bac8e34e7c6a27b31aad0a1ad5a1c21098f214
Christian Stocker <me@chregu.tv>
2011-06-03 12:36:24 +0200

    prepare for 2.2.8-SNAPSHOT

--- upload_to_S3.sh
+++ upload_to_S3.sh
@@ -1,7 +1,10 @@
 START=$PWD
+NAME=jackrabbit-standalone-2.2.8-jackalope-SNAPSHOT.jar
 DIR=$( (cd -P $(dirname $0) && pwd) )
 cd $DIR
-php upload_file.php ../jackrabbit-standalone/target/jackrabbit-standalone-2.2.7-jackalope-SNAPSHOT.jar jackrabbit-standalone-2.2.7-jackalope-SNAPSHOT.jar "application/x-gzip"
+php upload_file.php ../jackrabbit-standalone/target/$NAME $NAME "application/x-gzip"
 php upload_file.php index.html index.html
+
+echo "The uploaded file can be found at http://patched-jackrabbit.s3-website-eu-west-1.amazonaws.com/$NAME"
 
 cd $START
