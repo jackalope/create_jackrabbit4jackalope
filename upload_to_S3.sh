9f4582a8f03631dd986654eb7a393e84ab00caf7
Christian Stocker <me@chregu.tv>
2012-01-25 07:25:55 +0100

    added correct mimetype

--- upload_to_S3.sh
+++ upload_to_S3.sh
@@ -1,9 +1,9 @@
 START=$PWD
-NAME=jackrabbit-standalone-2.2.8-jackalope-SNAPSHOT.jar
+NAME=jackrabbit-standalone-2.4-jackalope-SNAPSHOT.jar
 DIR=$( (cd -P $(dirname $0) && pwd) )
 cd $DIR
 php upload_file.php ../jackrabbit-standalone/target/$NAME $NAME "application/x-gzip"
-php upload_file.php index.html index.html
+php upload_file.php index.html index.html text/html
 
 echo "The uploaded file can be found at http://patched-jackrabbit.s3-website-eu-west-1.amazonaws.com/$NAME"
 
